ecba74b49b8fdf2afc1061f9b2c56baf84f2a38d
burnsa721 <burnsa721@gmail.com>
2014-04-30 15:40:23 -0500

    init

---
+++ OceanCreatures/main.js
@@ -0,0 +1,24 @@
+﻿var myApp = {};
+
+myApp.OceanCreatures = [];
+
+myApp.OceanCreatures = function () {
+    this.name = name;
+    this.size = size;
+    this.img = img;
+}
+
+myApp.createCreature = function () {
+
+
+}
+
+myApp.deleteCreature = function (key) {
+
+    var request = new XMLHttpRequest();
+
+    request.open('DELETE', myApp.url + key + '.json', true);
+
+    request//and continue one with normal ajax request
+
+}
